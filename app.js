971ff239a192438aa4a1a237f0d27220dde1a545
Francisca Cifuentes Riquelme <fraans.cifuentes@gmail.com>
2024-06-27 21:36:47 -0400

    Upload Project Portafolio

---
+++ app.js
@@ -0,0 +1,52 @@
+/*
+    Con Emmet instalado, puedes comenzar a utilizar sus abreviaturas para 
+    acelerar el desarrollo. Algunas abreviaturas comunes incluyen:
+
+    html: Genera la estructura básica de un documento HTML.
+    ul>li*5: Crea una lista no ordenada con cinco elementos de lista.
+    div>h1+p: Crea una div que contiene un encabezado (h1) seguido de un párrafo (p).
+    input[type=text]+input[type=password]: Crea dos campos de entrada, uno para texto y otro para contraseña.
+*/
+
+/*
+    Aprendiste a destacar el texto utilizando la etiqueta <strong>, sin embargo,
+    hay otra etiqueta que también se utiliza mucho para esto, que es la etiqueta <span>. 
+    A diferencia de <strong>, <span> no aplica negrita por defecto, pero es una excelente 
+    forma de resaltar partes del texto en HTML. Mira:
+
+    HTML
+    <h1>Formación de <span>Front-end</span></h1>
+
+    CSS
+
+    h1{
+        font-weight: bold;
+    }
+
+    span{
+        color: #22D4FD;
+        border: 1px solid #22D4FD;
+        padding: 10px;
+    }
+ */
+
+/*
+    como crear clases en css y html
+    primero en html por ejemplo quiero una clase en <strong></strong>
+    debo agregar asi: <strong class="tituloPrincipal">con un Front-end de calidad!</strong>
+    y en css llamar SIEMPRE con un punto:
+
+    .tituloPrincipal{
+        color: #22D4FD;
+    }
+    
+    Se utilizan las clases para diferencia mismas etiquetas, el ejemplo de arriba ya teniamos otro strong y 
+    por eso introducimos una clase para dar color a cada strong
+*/
+
+/*
+    Propiedades align-items y justify-content.
+
+    ¡Correcto! Ambas propiedades forman parte de la estructura de Flexbox y 
+    actúan en el elemento padre con la responsabilidad de espaciar los elementos hijos. 
+*/
